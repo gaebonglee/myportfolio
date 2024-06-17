7ded2568d480b7809eba2f2279b9357e28f5e07b
gayoung lee <gost1073@gmail.com>
2024-06-17 14:35:50 +0900

    copymodules 수정

--- copy-modules.js
+++ copy-modules.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const modules = ["exchange_currency"];
 
 modules.forEach((module) => {
-  if (fs.existsSync(module)) {
+  if (fs.existsSync(`./${module}`)) {
     exec(`robocopy ${module} build\\${module} /E`, (error, stdout, stderr) => {
       if (error) {
         console.error(`Error copying ${module}:`, error);
