94933114fcc1e125c0f56cd21fe11375f8627909
gayoung lee <gost1073@gmail.com>
2024-06-17 14:25:09 +0900

    node파일추가

---
+++ copy-modules.js
@@ -0,0 +1,18 @@
+const { exec } = require("child_process");
+const fs = require("fs");
+
+const modules = ["exchange_currency"];
+
+modules.forEach((module) => {
+  if (fs.existsSync(module)) {
+    exec(`robocopy ${module} build\\${module} /E`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error copying ${module}:`, error);
+        return;
+      }
+      console.log(`Copied ${module} successfully.`);
+    });
+  } else {
+    console.warn(`Module ${module} does not exist.`);
+  }
+});
