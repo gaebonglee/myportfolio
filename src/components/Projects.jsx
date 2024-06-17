4f5029a6f0eec15f563640e8ceeb1ab2bc6d3f78
gayoung lee <gost1073@gmail.com>
2024-06-17 15:56:41 +0900

    경로수정1

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -40,7 +40,8 @@ const projects = [
     tech: "HTML5, CSS3, JavaScript",
     mywork: "HTML 마크업, CSS 스타일링, JavaScript 기능구현",
     image: `${process.env.PUBLIC_URL}/image/project/exchange_currency.png`,
-    projectLink: `/myportfolio/exchangeCurrency/index.html`,
+    projectLink:
+      "https://gaebonglee.github.io/myportfolio/exchangeCurrency/index.html",
     githubLink: "https://github.com/gaebonglee/exchangeCurrency.git",
   },
 ];
