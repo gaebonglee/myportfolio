9ccdc234d5bad251b11efbf3e6bd05290552a583
gayoung lee <gost1073@gmail.com>
2024-06-23 01:26:29 +0900

    수정중 종료 전 업데이트

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-// import AboutMeBack from "../components/aboutme/AboutMeBack";
 import AboutMeText from "../components/aboutme/AboutMeText";
 
 const AboutMe = () => {
   return (
     <section className="AboutMe_section">
       <div className="pages">
-        {/* <AboutMeBack /> */}
         <AboutMeText />
       </div>
     </section>
